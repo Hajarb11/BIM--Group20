6be66aec08e2ec8998934eeff8d781739198bb24
s222394 <112948104+s222394@users.noreply.github.com>
2022-09-25 22:40:02 +0200

    Update and rename Script for Blender to Script_Blender.py

--- Script_Blender.py
+++ Script_Blender.py
@@ -1,25 +1,28 @@
-### Import libraries to allow blender to work with Python
-### Importing function from blenderbim library to store file, and to allow us to extract information from the model we've opened
+### Import libraries to allow blender to work with Python ###
+### Importing function from blenderbim library to store file, and to allow us to extract information from the model we've opened ###
 
 import bpy
 from blenderbim.bim.ifc import IfcStore
 
-### Function to store file in a variable called 'file'
+### Function to store file in a variable called 'file' ###
 file = IfcStore.get_file()
 
-### Functions to define various structural elements in the model
+### STRUCTURAL ELEMENTS VARIABLE DEFINITION ###
+
 beams = file.by_type('IfcBeam')
 walls = file.by_type('IfcWall')
 columns = file.by_type('IfcColumn')
 slabs = file.by_type('IfcSlab')
 
-###Function Used to count the number of Structural Elements
+### COUNT STRUCTURAL ELEMENTS ###
+
 print('Number of beams = {length}'.format(length=len(beams)))
 print('Number of walls = {length}'.format(length=len(walls)))
 print('Number of columns = {length}'.format(length=len(columns)))
 print('Number of slabs = {length}'.format(length=len(slabs))) 
 
-###Function that prints the attributes for the first five beams in the project. The same can be repeated for all other elements
+### ATTRIBUTES FOR FIRST FIVE BEAMS ###
+
 count = 1
 
 for beam in beams[:6]:
@@ -31,25 +34,26 @@ for beam in beams[:6]:
     print('\n \n')
     count+=1
     
-###Writing a function to get "Names" of all the properties associated with a beam
+### FUNCTION FOR PROPERTY SET "NAME" IN BEAM ###
 
 for beam in beams:
-###Using for loop to search the column/array 'IsDefinedBy' which contains property sets for all types of elements
+##Using for loop to search the column/array 'IsDefinedBy' which contains property sets for all types of elements
     for definition in beams.IsDefinedBy:
-    ###Using if function to filter properties which we need 
+    ##Using if function to filter properties which we need 
         if definition.is_a('IfcRelDefinesByProperties'):
             property_set = definition.RelatingPropertyDefinition
             print(property_set.Name)
             
             
-###Writing a function to get "Names" of all the properties associated with a beam. Using these names, we can output any property we desire
+### NAMES OF ALL THE PROPERTIES ASSOCIATED WITH A BEAM ###
 
 for beam in beams:
     for definition in beam.IsDefinedBy:
         for properties in definition.RelatingPropertyDefinition.HasProperties:
             print(properties.Name)
         
-###Writing a function to get total combined length of all beams in the file
+### TOTAL BEAM LENGTH IN PROJECT ###
+
 total_beam_length = 0
 
 for beam in beams:
@@ -59,4 +63,10 @@ for beam in beams:
                 total_beam_length += properties.NominalValue.wrappedValue
 
 print('The total length of all the beams in the project is: {}'.format(total_beam_length))
+
+### MATERIAL PROPERTIES OF A BEAM ### 
+
+for beam in beams:
+    for relAssociatesMaterial in beam.HasAssociations:
+        print(relAssociatesMaterial.RelatingMaterial.Name)
 
